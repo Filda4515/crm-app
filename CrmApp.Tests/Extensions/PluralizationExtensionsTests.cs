84ac0bdb9ca05a0468c0e8d8e63e89951f69bc7c
Filda 4515 <filip4515@gmail.com>
2026-06-07 13:21:33 +0200

    feat: add contract pluralization logic

---
+++ CrmApp.Tests/Extensions/PluralizationExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using CrmApp.Extensions;
+
+namespace CrmApp.Tests.Extensions;
+
+public class PluralizationExtensionsTests
+{
+    [Theory]
+    [InlineData(0, "smluv")]
+    [InlineData(1, "smlouvu")]
+    [InlineData(2, "smlouvy")]
+    [InlineData(3, "smlouvy")]
+    [InlineData(4, "smlouvy")]
+    [InlineData(5, "smluv")]
+    [InlineData(10, "smluv")]
+    [InlineData(100, "smluv")]
+    public void PluralizeContracts_ShouldReturnCorrectForm_ForGivenCount(int count, string expected)
+    {
+        // Act
+        var result = count.PluralizeContracts();
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
---
+++ CrmApp/Extensions/PluralizationExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace CrmApp.Extensions;
+
+public static class PluralizationExtensions
+{
+    public static string PluralizeContracts(this int count)
+    {
+        return count switch
+        {
+            1 => "smlouvu",
+            2 or 3 or 4 => "smlouvy",
+            _ => "smluv"
+        };
+    }
+}
